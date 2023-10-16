00bd5ae4abb12e8235c12906abce8ee89aca940c
pedrovidal <rm93567@fiap.com.br>
2023-10-16 18:58:18 -0300

    Criando projeto

---
+++ schooltask/src/main/java/br/com/fiap/schooltask/SchooltaskApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.schooltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooltaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooltaskApplication.class, args);
+	}
+
+}
---
+++ schooltask/src/test/java/br/com/fiap/schooltask/SchooltaskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.schooltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooltaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
