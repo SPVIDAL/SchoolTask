c83f56ffca0881f86e20ec4d0f4df441370d991b
pedrovidal <rm93567@fiap.com.br>
2023-10-16 19:50:26 -0300

    Project login social

---
+++ schooltask/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO TASK (title, description, score) VALUES ('Prototipo', 'Figma', 50);
+INSERT INTO TASK (title, description, score, status) VALUES ('BD', 'Banco de dados da aplicação', 30, 40);
